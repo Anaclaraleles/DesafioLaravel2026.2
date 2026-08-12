8ea3d37fe40877e168dbdb1935c6245b975f31a1
Ana Clara <anaclaralelesquintao@gmail.com>
2026-08-12 11:21:26 -0300

    fix: ajusta a proteção das rotas

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
 });
 
 Route::middleware('auth')->group(function (): void {
+
     Route::middleware(User::class)->group(function (): void {
         Route::prefix('user')->group(function (): void {
             Route::post('/logout', [LoginController::class, 'logout'])->name('user.logout');
@@ -31,9 +32,7 @@ Route::middleware('auth')->group(function (): void {
             Route::get('/vendas', fn () => 'Histórico de vendas (em construção)')->name('user.vendas');
         });
     });
-});
 
-Route::middleware('auth')->group(function (): void {
     Route::middleware(Admin::class)->group(function (): void {
         Route::prefix('admin')->group(function (): void {
             Route::post('/logout', [LoginController::class, 'logout'])->name('admin.logout');
@@ -49,17 +48,15 @@ Route::middleware('auth')->group(function (): void {
             Route::get('/vendas', fn () => 'Histórico de vendas (em construção)')->name('admin.vendas');
         });
     });
+
+    Route::get('/products/manage', [ProductController::class, 'manage'])->name('products.manage');
+    Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+    Route::put('/products/{product}/edit', [ProductController::class, 'update']);
+    Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
+    Route::get('/product/{id}', [ProductController::class, 'detail'])->name('product.detail');
+
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
 });
-
-            Route::get('/products/manage', [ProductController::class, 'manage'])->name('products.manage');
-            Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-            Route::put('/products/{product}/edit', [ProductController::class, 'update']);
-            Route::delete('products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
-
-            Route::get('/product/{id}', [ProductController::class, 'detail'])->name('product.detail');
-
-            Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
-            Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-
-
 //require __DIR__.'/auth.php';
